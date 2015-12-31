d25c901fe4d1ee3dab308b7e6eb7325cec2fd2f5
Elias Aebi <user142@hotmail.com>
2015-12-31 13:35:23 +0100

    add a wayland SHM example

--- wayland-shm.c
+++ wayland-shm.c
@@ -1,36 +1,44 @@
-// gcc -o wayland-egl wayland-egl.c -lwayland-client -lwayland-egl -lEGL -lGL
+// gcc -o wayland-shm wayland-shm.c -lwayland-client
 
+#define _GNU_SOURCE // for O_TMPFILE
 #include <wayland-client.h>
-#include <wayland-egl.h>
-#include <EGL/egl.h>
-#include <GL/gl.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdio.h>
 
 #define WIDTH 256
 #define HEIGHT 256
 
+#define EXIT(msg) { fputs (msg, stderr); exit (EXIT_FAILURE); }
+
 static struct wl_display *display;
 static struct wl_compositor *compositor = NULL;
 static struct wl_shell *shell = NULL;
-static EGLDisplay egl_display;
+static struct wl_shm *wl_shm = NULL;
 static char running = 1;
 
 struct window {
-	EGLContext egl_context;
+	int32_t width, height;
 	struct wl_surface *surface;
 	struct wl_shell_surface *shell_surface;
-	struct wl_egl_window *egl_window;
-	EGLSurface egl_surface;
+	struct wl_buffer *front_buffer;
+	int32_t *data;
 };
 
 // listeners
 static void registry_add_object (void *data, struct wl_registry *registry, uint32_t name, const char *interface, uint32_t version) {
-	if (!strcmp(interface,"wl_compositor")) {
+	printf ("registry_add_object: %s\n", interface);
+	if (strcmp(interface,"wl_compositor") == 0) {
 		compositor = wl_registry_bind (registry, name, &wl_compositor_interface, 0);
 	}
 	else if (!strcmp(interface,"wl_shell")) {
 		shell = wl_registry_bind (registry, name, &wl_shell_interface, 0);
+	}
+	else if (!strcmp(interface,"wl_shm")) {
+		wl_shm = wl_registry_bind (registry, name, &wl_shm_interface, 0);
 	}
 }
 static void registry_remove_object (void *data, struct wl_registry *registry, uint32_t name) {
@@ -38,12 +46,19 @@ static void registry_remove_object (void *data, struct wl_registry *registry, ui
 }
 static struct wl_registry_listener registry_listener = {&registry_add_object, &registry_remove_object};
 
+static void surface_enter (void *data, struct wl_surface *wl_surface, struct wl_output *output) {
+	
+}
+static void surface_leave (void *data, struct wl_surface *wl_surface, struct wl_output *output) {
+	
+}
+static struct wl_surface_listener surface_listener = {&surface_enter, &surface_leave};
+
 static void shell_surface_ping (void *data, struct wl_shell_surface *shell_surface, uint32_t serial) {
 	wl_shell_surface_pong (shell_surface, serial);
 }
 static void shell_surface_configure (void *data, struct wl_shell_surface *shell_surface, uint32_t edges, int32_t width, int32_t height) {
-	struct window *window = data;
-	wl_egl_window_resize (window->egl_window, width, height, 0, 0);
+	
 }
 static void shell_surface_popup_done (void *data, struct wl_shell_surface *shell_surface) {
 	
@@ -51,36 +66,40 @@ static void shell_surface_popup_done (void *data, struct wl_shell_surface *shell
 static struct wl_shell_surface_listener shell_surface_listener = {&shell_surface_ping, &shell_surface_configure, &shell_surface_popup_done};
 
 static void create_window (struct window *window, int32_t width, int32_t height) {
-	eglBindAPI (EGL_OPENGL_API);
-	EGLint attributes[] = {
-		EGL_RED_SIZE, 8,
-		EGL_GREEN_SIZE, 8,
-		EGL_BLUE_SIZE, 8,
-	EGL_NONE};
-	EGLConfig config;
-	EGLint num_config;
-	eglChooseConfig (egl_display, attributes, &config, 1, &num_config);
-	window->egl_context = eglCreateContext (egl_display, config, EGL_NO_CONTEXT, NULL);
-	
+	window->width = width;
+	window->height = height;
 	window->surface = wl_compositor_create_surface (compositor);
+	wl_surface_add_listener (window->surface, &surface_listener, NULL);
 	window->shell_surface = wl_shell_get_shell_surface (shell, window->surface);
-	wl_shell_surface_add_listener (window->shell_surface, &shell_surface_listener, window);
+	wl_shell_surface_add_listener (window->shell_surface, &shell_surface_listener, NULL);
 	wl_shell_surface_set_toplevel (window->shell_surface);
-	window->egl_window = wl_egl_window_create (window->surface, width, height);
-	window->egl_surface = eglCreateWindowSurface (egl_display, config, window->egl_window, NULL);
-	eglMakeCurrent (egl_display, window->egl_surface, window->egl_surface, window->egl_context);
+	
+	size_t size = width * height * 4;
+	char *xdg_runtime_dir = getenv ("XDG_RUNTIME_DIR");
+	if (!xdg_runtime_dir) EXIT ("XDG_RUNTIME_DIR not set\n");
+	int fd = open (xdg_runtime_dir, O_TMPFILE|O_RDWR|O_EXCL, 0600);
+	ftruncate (fd, size);
+	window->data = mmap (NULL, size, PROT_READ|PROT_WRITE, MAP_SHARED, fd, 0);
+	struct wl_shm_pool *pool = wl_shm_create_pool (wl_shm, fd, size);
+	window->front_buffer = wl_shm_pool_create_buffer (pool, 0, width, height, width*4, WL_SHM_FORMAT_XRGB8888);
+	wl_shm_pool_destroy (pool);
+	close (fd);
 }
 static void delete_window (struct window *window) {
-	eglDestroySurface (egl_display, window->egl_surface);
-	wl_egl_window_destroy (window->egl_window);
 	wl_shell_surface_destroy (window->shell_surface);
 	wl_surface_destroy (window->surface);
-	eglDestroyContext (egl_display, window->egl_context);
 }
 static void draw_window (struct window *window) {
-	glClearColor (0.0, 1.0, 0.0, 1.0);
-	glClear (GL_COLOR_BUFFER_BIT);
-	eglSwapBuffers (egl_display, window->egl_surface);
+	int x, y;
+	for (y = 0; y < window->height; y++) {
+		for (x = 0; x < window->width; x++) {
+			int index = y * window->width + x;
+			window->data[index] = 0xFF00FF00; // XRGB format -> green
+		}
+	}
+	wl_surface_attach (window->surface, window->front_buffer, 0, 0);
+	//wl_surface_damage (window->surface, 0, 0, window->width, window->height);
+	wl_surface_commit (window->surface);
 }
 
 int main () {
@@ -89,19 +108,14 @@ int main () {
 	wl_registry_add_listener (registry, &registry_listener, NULL);
 	wl_display_roundtrip (display);
 	
-	egl_display = eglGetDisplay (display);
-	eglInitialize (egl_display, NULL, NULL);
-	
 	struct window window;
 	create_window (&window, WIDTH, HEIGHT);
 	
-	while (running) {
-		wl_display_dispatch_pending (display);
-		draw_window (&window);
-	}
+	draw_window (&window);
+	while (running)
+		wl_display_dispatch (display);
 	
 	delete_window (&window);
-	eglTerminate (egl_display);
 	wl_display_disconnect (display);
 	return 0;
 }
